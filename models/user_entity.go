46dc7a815b0f15993103a972d3de9890340edee3
cngirwa <cngirwa2007@gmail.com>
2022-01-10 17:41:51 +0300

    Update user_entity.go

--- models/user_entity.go
+++ models/user_entity.go
@@ -14,6 +14,7 @@ type User struct {
 	Password        string    `json:"password,omitempty" form:"password"`
 	LoginAttempt    int32     `json:"login_attempt,omitempty" form:"login_attempt"`
 	RememberToken   string    `json:"remember_token,omitempty" form:"remember_token"`
+	Hospital        *Hospital `json:"hospital"`
 	CreatedAt       time.Time `json:"created_at,omitempty"`
 	UpdatedAt       time.Time `json:"updated_at,omitempty"`
 	DeletedAt       time.Time `json:"deleted_at,omitempty"`
